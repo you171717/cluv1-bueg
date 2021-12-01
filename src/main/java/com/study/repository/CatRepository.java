b049a8ffdcb2f3eb7169625eb9f59cb38e5b5989
DESKTOP-AE8EH9T\YHJ <you171717@gsitm.com>
2021-12-01 13:21:04 +0900

    불필요 코드 제거, 프로젝트명 재설정

--- src/main/java/com/study/repository/CatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.repository;
-
-import com.study.entity.Cat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatRepository extends JpaRepository<Cat, Integer> {
-
-}
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `mybatis` (`id`, `email`)
-VALUES (1, 'mybatis@gsitm.com');
-
-INSERT INTO `mybatis2` (`id`, `email`)
-VALUES (1, 'mybatis@gsitm.com');
